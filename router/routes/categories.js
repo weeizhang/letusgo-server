2695a921ea1e894886a3f796dcbbcfaf5b1ee36b
lzhangwei <zhangwei_999@foxmail.com>
2014-09-18 23:35:58 +0800

    add load categories function

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,3 +1,13 @@
+function loadCategories(){
+  var categories = [
+    {id: 1, name: '饮料'},
+    {id: 2, name: '水果'},
+    {id: 3, name: '生活用品'},
+    {id: 4, name: '食品'}
+  ];
+  return categories;
+}
+
 var express = require('express');
 var router = express.Router();
 
