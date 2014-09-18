b71494ea7d7419213efd486ac63f9739a76e7ae7
lzhangwei <zhangwei_999@foxmail.com>
2014-09-18 23:54:24 +0800

    update index.js

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/items', require('./routes/items'));
+  app.use('/api/cartItems', require('./routes/cartItems'));
 };
