1cada9d8fc1539ce13e1ada50bece4728d560e6d
lzhangwei <zhangwei_999@foxmail.com>
2014-09-20 17:46:42 +0800

    formate codes

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,3 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var redis = require("redis");
+var client = redis.createClient();
+
 function loadCategories() {
   var categories = [
     {id: 1, name: '饮料'},
@@ -8,31 +13,20 @@ function loadCategories() {
   return categories;
 }
 
-var express = require('express');
-var router = express.Router();
-
-var redis = require("redis");
-var client = redis.createClient();
-
 var categories = loadCategories();
 client.set('categories', JSON.stringify(categories));
 
 router.get('/', function (req, res) {
-
   client.get('categories', function (err, obj) {
     res.send(obj);
   });
-
 });
 
 router.post('/', function (req, res) {
-
   var categories = req.param('categories');
-
   client.set('categories', categories, function (err, obj) {
     res.send(obj);
   });
-
 });
 
 module.exports = router;
