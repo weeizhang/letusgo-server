5fe366ea3c841391d89e0d9b60f9e005c3faf35b
lzhangwei <zhangwei_999@foxmail.com>
2014-09-20 17:47:23 +0800

    update get param

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -1,25 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
 var redis = require("redis");
 var client = redis.createClient();
 
 router.get('/', function (req, res) {
-
   client.get('cartItems', function (err, obj) {
     res.send(obj);
   });
-
 });
 
 router.post('/', function (req, res) {
-
   var cartItems = req.param('cartItems');
-
-  client.set('cartItems', cartItems, function (err, obj) {
+  client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
     res.send(obj);
   });
-
 });
 
 module.exports = router;
