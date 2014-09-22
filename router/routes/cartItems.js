f33e56f803220c17011c8fec6d418a1a70f5be88
lzhangwei <zhangwei_999@foxmail.com>
2014-09-22 23:58:01 +0800

    update post

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -4,6 +4,15 @@ var redis = require("redis");
 var client = redis.createClient();
 var _ = require('lodash');
 
+function addCartItem(cartItem, callback) {
+  client.get('cartItems', function (err, obj) {
+    obj = JSON.parse(obj);
+    cartItem.id = obj[obj.length - 1].id + 1;
+    obj[obj.length] = cartItem;
+    callback(obj);
+  });
+}
+
 router.get('/', function (req, res) {
   client.get('cartItems', function (err, obj) {
     res.send(obj);
@@ -11,9 +20,11 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-  var cartItems = req.param('cartItems');
-  client.set('cartItems', JSON.stringify(cartItems), function (err, obj) {
-    res.send(obj);
+  var cartItem = req.param('cartItem');
+  addCartItem(cartItem, function (data) {
+    client.set('cartItems', JSON.stringify(data), function (err, obj) {
+      res.send(data);
+    });
   });
 });
 
